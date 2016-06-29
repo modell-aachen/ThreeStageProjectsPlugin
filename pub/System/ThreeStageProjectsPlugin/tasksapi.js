e5f242ead0a22797f7b038fe2d084d7dcee10dce
Sven Meyer <meyer@modell-aachen.de>
2016-06-29 10:43:26 +0200

    Bug fixes

--- pub/System/ThreeStageProjectsPlugin/tasksapi.js
+++ pub/System/ThreeStageProjectsPlugin/tasksapi.js
@@ -3,6 +3,12 @@
     var opts = $(this).data('tasktracker_options');
     var query = $.parseJSON(opts.query);
     task.Milestone = query.Milestone;
+
+    var p = foswiki.preferences;
+    if (task.Context == 'any' && /Base$/.test(p.TOPIC)) {
+      task.Context = p.WEB + '.' + p.TOPIC.replace('Base', task.Milestone);
+    }
+
     return task;
   };
 
